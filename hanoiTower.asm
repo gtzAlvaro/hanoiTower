65244d4146532c29d3c42feb1ba373c4abe1cb1d
gtzAlvaro <gutierrez.arce.alvaro@gmail.com>
2019-02-22 10:47:16 -0600

    created condition to exit recursive function

--- hanoiTower.asm
+++ hanoiTower.asm
@@ -7,14 +7,23 @@
 
 .text
 main:
-	addi $a0, $a0, 4
+	addi $a0, $a0, 1
 	addi $a1, $a1, 1
 	addi $a2, $a2, 3
 	addi $a3, $a3, 2
+	addi $t0, $t0, 1
 	jal hanoiTower
 	j exit
 
 hanoiTower:
+	bne $a0, $t0, else
+	andi $t1, $t2, 0
+	andi $t2, $t2, 0
+	add $t1, $t1, $a1
+	add $t2, $t2, $a2
+	jr $ra
+else:
+	
 	jr $ra	
 
 exit:
