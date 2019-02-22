4243f8dd1989c5dd2e13135348d287762c14199c
gtzAlvaro <gutierrez.arce.alvaro@gmail.com>
2019-02-22 11:15:58 -0600

    store return addresses in stack

--- hanoiTower.asm
+++ hanoiTower.asm
@@ -7,7 +7,7 @@
 
 .text
 main:
-	addi $a0, $a0, 1
+	addi $a0, $a0, 3
 	addi $a1, $a1, 1
 	addi $a2, $a2, 3
 	addi $a3, $a3, 2
@@ -23,7 +23,27 @@ hanoiTower:
 	add $t2, $t2, $a2
 	jr $ra
 else:
+	addi $a0, $a0, -1
+	andi $a1, $a1, 0
+	andi $a2, $a2, 0
+	andi $a3, $a3, 0
+	addi $a1, $a1, 1
+	addi $a2, $a2, 2
+	addi $a3, $a3, 3
+	sw $ra, ($sp)
+	addi $sp, $sp, -4
+	jal hanoiTower	
 	
-	jr $ra	
-
+	andi $t1, $t2, 0
+	andi $t2, $t2, 0
+	add $t1, $t1, $a1
+	add $t2, $t2, $a2
+	
+	andi $a1, $a1, 0
+	andi $a2, $a2, 0
+	andi $a3, $a3, 0
+	addi $a1, $a1, 3
+	addi $a2, $a2, 2
+	addi $a3, $a3, 1
+	jal hanoiTower
 exit:
