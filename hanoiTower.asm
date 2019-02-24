0a0e7cc1ce7b3c0440f12ac8da5ee17efce3ed2e
gtzAlvaro <gutierrez.arce.alvaro@gmail.com>
2019-02-23 18:55:34 -0600

    draw  number of disks in the first column in data segment

--- hanoiTower.asm
+++ hanoiTower.asm
@@ -2,25 +2,29 @@
 #Description:	hanoiTowers
 #Date:		2-22-19
 
-.data
-
-
 .text
 main:
-	addi $a0, $a0, 8	#total number of disks
+	addi $a0, $a0, 3	#total number of disks
 	addi $a1, $a1, 1	#origin rod
 	addi $a2, $a2, 3	#destiny rod
 	addi $a3, $a3, 2	#temporary rod
 	addi $t0, $t0, 1	#value used to compare when number of disks is reduced to one
+	add $t1, $t1, $a0	#value used to draw disk in data segment
+	addi $s0, $s0, 0x10010000
+	addi $s1, $s1, 0x10010020
+	addi $s2, $s2, 0x10010040
+draw: 
+	sw $t1, ($s0)
+	addi $t1, $t1, -1
+	addi $s0, $s0, 4
+	bne $t1, $zero, draw
+	
 	jal hanoiTower
 	j exit
 
 hanoiTower:
 	bne $a0, $t0, else	#if number of disks is not equal to one then store parameter values and call function again
-	andi $t1, $t1, 0	#clear t1
-	add $t1, $t1, $a1	#show the rod to move from in t1
-	andi $t2, $t2, 0	#clear t2
-	add $t2, $t2, $a2	#show the rod to move to in t2
+
 	jr $ra
 else:
 	addi $sp, $sp, -16	#save space in stack for 5 data packages
@@ -44,11 +48,8 @@ else:
 	lw $a2, 12($sp)		#load destiny rod from parent function
 	lw $a3, 16($sp)		#load temporary rod from parent function
 	lw $ra, 20($sp)		#load return address from parent function
-	
-	andi $t1, $t1, 0	#clear t1
-	add $t1, $t1, $a1	#show the rod to move from in t1
-	andi $t2, $t2, 0	#clear t2
-	add $t2, $t2, $a2	#show the rod to move to in t2
+
+
 	
 	addi $sp, $sp, -4	#save one extra sapce in stack to change values from origin rod and temporary rod
 	addi $a0, $a0, -1	#reduce the number of disks by one
