056c44867a9c686eb57d00f95eb9c7e67f279b34
gtzAlvaro <gutierrez.arce.alvaro@gmail.com>
2019-02-28 20:26:30 -0600

    added final comments

--- hanoiTower.asm
+++ hanoiTower.asm
@@ -8,23 +8,22 @@ main:
 	addi $a1, $a1, 1		#origin rod
 	addi $a2, $a2, 3		#destiny rod
 	addi $a3, $a3, 2		#temporary rod
-	addi $t0, $t0, 1
-	addi $s1, $s1, 0x10010000
-	addi $s2, $s2, 0x10010020
-	addi $s3, $s3, 0x10010040
+	addi $s1, $s1, 0x10010000	#value of data segment where origin rod will be drawn
+	addi $s2, $s2, 0x10010020	#value of data segment where temporary rod will be drawn
+	addi $s3, $s3, 0x10010040	#value of data segment where destiny rod will be drawn
 	addi $t1, $a0, 0
-while:
+while:					#print the number of disks in data segment
 	sw $t1, ($s1)
 	addi $s1, $s1, 4
 	addi $t1, $t1, -1
 	bne $t1, $zero, while
-	jal hanoiTower
+	jal hanoiTower			#solve hanoi tower
 	j exit
 
 hanoiTower:
-	bne $a0, $t0, else	#if number of disks is not equal to one then store parameter values and call function again
+	bne $a0, 1, else	#if number of disks is not equal to one then store parameter values and call function again
 	sw $ra, ($sp)
-	jal draw
+	jal draw		#move disks accrodingly
 	lw $ra, ($sp)
 	jr $ra
 else:
@@ -49,7 +48,7 @@ else:
 	lw $ra, 20($sp)		#load return address
 
 	sw $ra, ($sp)
-	jal draw
+	jal draw		#move disks accrodingly
 	lw $ra, ($sp)
 	
 	addi $a0, $a0, -1	#reduce the number of disks by one
@@ -57,41 +56,41 @@ else:
 	sll $a1, $a3, 0		#change destiny rod with temporary rod
 	sll $a3, $t1, 0
 	
-	jal hanoiTower
-	addi $sp, $sp, 20
-	lw $ra, ($sp)
+	jal hanoiTower		#call hanoi tower again
+	addi $sp, $sp, 20	#free memory from most recent to hanoi Tower
+	lw $ra, ($sp)		#load return address from most recent call to hanoi Tower
 	jr $ra
 
 draw:
-	bne $a1, 1, notOne
-	addi $s1, $s1, -4
-	lw $t1, ($s1)
-	sw $zero, ($s1)
+	bne $a1, 1, notOne	#if origin rod is 1
+	addi $s1, $s1, -4	#decrease origin rod pointer
+	lw $t1, ($s1)		#load disk
+	sw $zero, ($s1)		#remove disk
 	j destinyRod
 notOne:
-	bne $a1, 2, notTwo
-	addi $s2, $s2, -4
-	lw $t1, ($s2)
-	sw $zero, ($s2)
+	bne $a1, 2, notTwo	#if origin rod is 2
+	addi $s2, $s2, -4	#decrease origin rod pointer
+	lw $t1, ($s2)		#load disk
+	sw $zero, ($s2)		#remove disk
 	j destinyRod
 notTwo:	
-	addi $s3, $s3, -4
-	lw $t1, ($s3)
-	sw $zero, ($s3)
+	addi $s3, $s3, -4	#if origin rod is 3
+	lw $t1, ($s3)		#load disk
+	sw $zero, ($s3)		#remove disk
 	
 destinyRod:
-	bne $a2, 1, isTwo
-	sw $t1, ($s1)
-	addi $s1, $s1, 4
+	bne $a2, 1, isTwo	#if destiny rod is 1
+	sw $t1, ($s1)		#store disk
+	addi $s1, $s1, 4	#increase pointer
 	jr $ra
 isTwo:	
-	bne $a2, 2, isThree
-	sw $t1, ($s2)
-	addi $s2, $s2, 4
+	bne $a2, 2, isThree	#if destiny rod is 2
+	sw $t1, ($s2)		#store disk
+	addi $s2, $s2, 4	#increase pointer
 	jr $ra
 isThree:	
-	sw $t1, ($s3)
-	addi $s3, $s3, 4	
+	sw $t1, ($s3)		#if destiny rod is 3
+	addi $s3, $s3, 4	#store disk and increase pointer
 	jr $ra
 
 exit:
